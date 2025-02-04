609a938f54affe1bdf857499d81768befbb6ebee
Caroline Clark <carolineclark@Carolines-MacBook-Pro-3.local>
2025-02-04 09:27:16 -0500

    Added story components

---
+++ src/components/Content.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+const Content = () => {
+  return (
+    <div>
+      <p>Once upon a time,a young bird who is bullied and 
+        rejected by other animals because of his appearance. 
+        Born in a duck's nest, he is larger and different from his 
+        siblings, leading him to be ridiculed and mistreated.</p>
+      <p>He endures a harsh journey filled with loneliness and hardship until, 
+        one day, he matures into a beautiful swan.</p>
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+
+import React from "react";
+
+const Footer = () => {
+  return <p><strong>Moral of the story:</strong>Don't judge a book by its cover</p>;
+};
+
+export default Footer;
